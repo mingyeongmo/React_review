87c4ac52fe988dcf8376aff9485a229f03bd5da5
mingyeongmo <mingyeongmp@naver.com>
2021-07-03 23:52:21 +0900

    useEffect 첫 렌더링에만 호출

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React,{ useState } from 'react';
+import React,{ useState,useEffect } from 'react';
 
 
 const UseStateExample = () => {
   const [test, setTest] = useState('initail value');
-
+  const [test2, setTest2] = useState('initail value');
+  useEffect(() => {
+    console.log('test state에 대해서만 호출!');
+  },[]); // 첫 렌더링에만 호출
+  useEffect(() => {
+    console.log('test2 state에 대해서만 호출!');
+  },[test2]);
   return (
     <div>
       <p>{test}</p>
+      <p>{test2}</p>
       <input onChange={(e) => {setTest(e.target.value)}} />
-      
+      <input onChange={(e) => {setTest2(e.target.value)}} />
     </div>
   )
 }
@@ -17,7 +24,6 @@ function App() {
   return (
     <div>
       <UseStateExample />
-      <Name name="doit" color="red"/>
  
     </div>
     
@@ -25,14 +31,6 @@ function App() {
   
 }
 
-function Name({name,color}) {
-  return (
-    <div>
-      <div style = {{color}}>my name is {name}</div>
-    </div>
-    
-  )
-}
 
 
 
